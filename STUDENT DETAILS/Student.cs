3aab38661a92f9b348bb3f45b03a353841f1e65f
riskiekalimuda <riskie.kalimuda@gmail.com>
2020-08-09 07:35:18 +0700

    add student class and properties

---
+++ STUDENT DETAILS/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanSoal.STUDENT_DETAILS
+{
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Marks;
+        public override string ToString()
+        {
+            return First + " " +Last +" " + ID;
+        }
+    }
+}
