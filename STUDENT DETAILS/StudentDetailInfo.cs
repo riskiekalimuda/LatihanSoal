e9071f40c034afcf3fd137e8a5f513ca85a3d1e3
riskiekalimuda <riskie.kalimuda@gmail.com>
2020-08-09 11:49:05 +0700

    tambah file StudentDetailInfo.cs untuk menampung properti

---
+++ STUDENT DETAILS/StudentDetailInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanSoal.STUDENT_DETAILS
+{
+    public class StudentDetailInfo
+    {
+        #region properties
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int IDStudent { get; set; }
+        public int IDContact { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Identitas
+        {
+            get
+            {
+                return First + " " + Last + "-" + IDContact; 
+            }
+        }
+        public string Contact
+        {
+            get
+            {
+                return Email + "-" + Phone;
+            }
+        }
+        #endregion
+    }
+}
