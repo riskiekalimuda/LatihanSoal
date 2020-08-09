eda1696a508223e4c6be73976b05f9fc20138bf5
riskiekalimuda <riskie.kalimuda@gmail.com>
2020-08-09 07:34:28 +0700

    add file contact info and properties

---
+++ STUDENT DETAILS/ContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanSoal.STUDENT_DETAILS
+{
+    public class ContactInfo
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public override string ToString()
+        {
+            return Email + ", " + Phone;
+        }
+    }
+}
